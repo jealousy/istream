16e97d228a69fafbd3691e7c56b06a9957cb8a23
Nalin <nalindz@gmail.com>
2011-01-25 15:26:47 -0500

    alert stuff sort of working now

--- istream.cpp
+++ istream.cpp
@@ -6,16 +6,29 @@
 #include "istream.h"
 
 #include <boost/timer.hpp> 
-#include "libtorrent/entry.hpp"
-#include "libtorrent/bencode.hpp"
-#include "libtorrent/session.hpp"
+#include <libtorrent/entry.hpp>
+#include <libtorrent/bencode.hpp>
+#include <libtorrent/session.hpp>
+#include <libtorrent/alert.hpp>
+#include <libtorrent/alert_types.hpp>
+
+#include <vector>
 
 using namespace std;
 using namespace libtorrent;
+//using namespace boost::posix_time;
 
 int filename, hash_name, piece_id;
 
 //buffer calculations
+
+void displayStatus(torrent_handle th) {
+  torrent_status ts = th.status();
+  if (ts.state == 3) 
+    cout << "Downloading..." << endl;
+  else 
+    cout << ts.state << endl;
+}
 
 float numPiecesToBuffer(torrent_info ti, int mode, int timeToBuffer) {
   if (mode == MOVIE) {
@@ -29,11 +42,37 @@ float numPiecesToBuffer(torrent_info ti, int mode, int timeToBuffer) {
 }
 
 
-void setPriority(torrent_handle th, int numPieces) {
-  for (int i = 0; i < numPieces; i++) {
-
+void setPriority(torrent_handle th, int numPieces, int numBuffer) {
+  int i, priority;
+  i = 0;
+  for (priority = 7; priority >= 1; priority--) {
+    for (; i < numBuffer; i++) {
+      th.piece_priority(i, priority);
+    }
+    i++;
+    numBuffer += numBuffer;
   }
+}
 
+//void fillGaps(torrent_handle th, torrent_status ts, session s)  {
+//  for (int i = 10; i < ts.pieces.size(); i++) {
+//    bool comp = true;
+//    if (!ts.pieces[i]) {
+//      for (int j = i; i > (i - 10); j--) 
+//        if (!ts.pieces[j]) comp = false;
+//      if (comp) {
+//        th.read_piece(i - 1);
+//        alert* a = s.wait_for_alert(10);
+//        cout << a->what() << endl;
+//      }
+//    }
+//  }
+//}
+
+void printPriority(torrent_handle th) {
+  std::vector<int> prio = th.piece_priorities();
+  for(int i =0 ; i < prio.size(); i++)
+    cout << "piece: " << i << " prio: " << prio[i] << endl;
 }
 
 
@@ -51,20 +90,16 @@ void test() {
 
 int main(int argc, char* argv[]) {
 
-#if BOOST_VERSION < 103400
-	namespace fs = boost::filesystem;
-	fs::path::default_name_check(fs::no_check);
-#endif
-
-
   if (argc < 2) {
     cout << "Please specify a torrent to start" << endl;
   }
 
 	session s;
+
 	s.listen_on(std::make_pair(6881, 6889));
 	add_torrent_params p;
 	p.save_path = "./";
+  p.override_resume_data = true;
 	error_code ec;
 	p.ti = new torrent_info(argv[1], ec);
 
@@ -73,10 +108,14 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
+  s.set_alert_mask(alert::storage_notification);
+
+
+
 	torrent_handle th = s.add_torrent(p, ec);
 
   //sets sequential download
-  th.set_sequential_download(true);
+//  th.set_sequential_download(true);
 
   th.set_max_connections(40);
 
@@ -93,25 +132,88 @@ int main(int argc, char* argv[]) {
   int upload_rate = 0;
   int num_seeds = 0;
 
-  torrent_status ts = th.status();
   torrent_info ti = th.get_torrent_info();
   std::vector<int> availability;
 
+  int numPieces = ti.num_pieces();
   int numBuffer = numPiecesToBuffer(ti, MOVIE, 15); // buffer 15 secs
-
 
   cout << "Buffering: " << numBuffer << " pieces" << endl;
 
+  setPriority(th, numPieces, numBuffer);
+
+//  printPriority(th);
+
   while(1) {
-    if (t.elapsed() > 2) {
+    if (t.elapsed() > 5) {
+      torrent_status ts = th.status();
       download_rate = ts.download_rate / 1000;
       upload_rate = ts.upload_rate / 1000;
       num_seeds = ts.num_seeds;
+      bitfield pieces = ts.pieces;
 
-      cout << "\r Download Rate: " << download_rate << " kb/s Upload Rate: " 
-           << upload_rate << " kb/s Number of Connected Seeds: " << num_seeds << flush;
-      t.restart();
-    }
+      int dlpiece = -1;
+
+
+      if (dlpiece == -1) {
+        for(int i = 0; i < 15; i++) {
+          if (ts.pieces[i]) {
+            dlpiece = i;
+            break;
+          }
+        }
+      }
+
+      if (dlpiece != -1) {
+        cout << "CALLED READ" << endl;
+        bool found = false;
+
+        time_duration td = seconds(5);
+
+        th.read_piece(dlpiece);
+
+        const alert * a = s.wait_for_alert(td);
+        std::auto_ptr<alert> holder = s.pop_alert();
+        if (alert_cast<read_piece_alert>(a)) {
+          const read_piece_alert* rpa = alert_cast<read_piece_alert>(a);
+          cout << "Piece found: " << rpa->piece;
+        }
+      }
+
+
+      for (int i = 0; i < ts.pieces.size(); i++)
+        cout <<  ts.pieces[i] << flush;
+      cout << endl;
+
+//      fillGaps(th, ts, s);
+
+//        for (int i = 10; i < pieces.size(); i++) {
+//          bool comp = true;
+//          if (!ts.pieces[i]) {
+//            for (int j = (i- 1); j > (i - 5); j--) 
+//              if (!ts.pieces[j]) comp = false;
+//            if (comp) {
+//              cout << "Calling READ: " << endl;
+//              th.read_piece(i - 1);
+//              time_duration td = seconds(5);
+//              bool findAlert = false;
+//              while (!findAlert) {
+//                const alert * a = s.wait_for_alert(td);
+//                std::auto_ptr<alert> holder = s.pop_alert();
+//                if (strcmp(a->what(), "read_piece_alert"))
+//                  findAlert = true;
+//              cout << a->what() << endl;
+//              }
+//            }
+//          }
+//        }
+
+          //      displayStatus(th);
+
+          cout << "\r Download Rate: " << download_rate << " kb/s Upload Rate: " 
+            << upload_rate << " kb/s Number of Connected Seeds: " << num_seeds << " Status: " << ts.state << flush;
+          t.restart();
+      }
   }
 
 	return 0;
